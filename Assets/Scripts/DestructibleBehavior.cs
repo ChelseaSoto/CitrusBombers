30c3fb0109932a5109686e9079ffbd8470fd2008
aidane1023 <earnestaidan@gmail.com>
2024-01-31 16:57:40 -0500

    Pushing Movement
    Fixing controls for enemy movement

--- Assets/Scripts/DestructibleBehavior.cs
+++ Assets/Scripts/DestructibleBehavior.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestrucibleBehavior : MonoBehaviour
+public class DestructibleBehavior : MonoBehaviour
 {
     public float destructionTime = 1f;
 
-    private void Start()
+    public float powerupSpawnChance = 0.1f;
+    public int[] spawnLimits = { 2, 3 };
+    public GameObject[] spawnableItems;
+
+    void Start()
     {
         Destroy(gameObject, destructionTime);
+    }
+
+    private void OnDestroy()
+    {
+        if (spawnableItems.Length > 0 && Random.value < powerupSpawnChance)
+        {
+            int random = Random.Range(0, spawnableItems.Length);
+            if (spawnLimits[random] > 0)
+            {
+                Instantiate(spawnableItems[random], transform.position, Quaternion.identity);
+                spawnLimits[random]--;
+            }
+        }
     }
 }
--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,31 +4,48 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class EnemyBehavior : MonoBehaviour
-{
-    public GameObject knightPrefab;
+{   
     public Rigidbody2D rigidbody;
+    public Animator animator;
     public Vector2 movement;
     private Vector2 position;
     public LayerMask knightLayerMask;
 
     public float speed = 4f;
+    private float speedStashed;
     private bool changed = false;
+    
+    public int direction = 5;
 
     void Start()
     {   
+        speedStashed = speed;
         NewDirection();
 
         StartCoroutine(ChangeDirectionInterval());
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        transform.Translate(position + movement * speed * Time.fixedDeltaTime);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", speed);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {   
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {  
         if (other.tag == "Solid")
+        {
+            movement.x = -movement.x;
+            movement.y = -movement.y;
+        }
+
+        if (other.tag == "Player")
         {
             movement.x = -movement.x;
             movement.y = -movement.y;
@@ -37,13 +54,17 @@ public class EnemyBehavior : MonoBehaviour
 
     private void SetDirection(int direction)
     {
-        if (transform.position.x % 1 != 0f && transform.position.y % 1 != 0f)
+        position = rigidbody.position;
+
+        speed = 0;
+        movement = Vector2.zero;
+
+        if (position.x % 1 >= 0.0001f && transform.position.y % 1 >= 0.0001f)
         {
-            Debug.Log("failed to meet both: x divisible " + rigidbody.position.x % 1 +". y divisible " + rigidbody.position.y);
+            speed = speedStashed;
             return;
         }
-        
-        Debug.Log("x divisible " + rigidbody.position.x % 1 +". y divisible " + rigidbody.position.y);
+
         changed = true;
 
         switch (direction)
@@ -51,55 +72,52 @@ public class EnemyBehavior : MonoBehaviour
             case 0:
                 movement.x = 1;
                 movement.y = 0;
-                Debug.Log("Moving Right");
                 break;
             case 1:
                 movement.x = 0;
                 movement.y = 1;
-                Debug.Log("Moving Up");
                 break;
             case 2:
                 movement.x = -1;
                 movement.y = 0;
-                Debug.Log("Moving Left");
                 break;
             case 3:
                 movement.x = 0;
                 movement.y = -1;
-                Debug.Log("Moving Down");
                 break;
         }
+        
+        speed = speedStashed;
     }
 
     private void NewDirection()
-    {
-        position = transform.position;
+    {   
+        position = rigidbody.position;
+
         if (movement.x == 0)
         {
-            Debug.Log("Checking x axis");
             if (CheckAxis(position, Vector2.right))
             {
-                Debug.Log("Returned true to open right");
-                SetDirection(0);
+                direction = 0;
+                SetDirection(direction);
             }
             else if (CheckAxis(position, Vector2.left))
             {
-                Debug.Log("Returned true to open left");
-                SetDirection(2);
+                direction = 2;
+                SetDirection(direction);
             }
         }
-        else if (movement.y == 0)
+        if (movement.y == 0)
         {
-            Debug.Log("Checking y axis");
             if (CheckAxis(position, Vector2.up))
             {
-                Debug.Log("Returned true to open up");
-                SetDirection(1);
+                direction = 1;
+                SetDirection(direction);
             }
             else if (CheckAxis(position, Vector2.down))
             {
-                Debug.Log("Returned true to open down");
-                SetDirection(3);
+                direction = 3;
+                SetDirection(direction);
             }
         }
     }
@@ -119,6 +137,7 @@ public class EnemyBehavior : MonoBehaviour
     IEnumerator ChangeDirectionInterval()
     {
         changed = false;
+
         yield return new WaitForSeconds(10f);
 
         while (!changed)
