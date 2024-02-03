f0d7c2bf12ae8b22822d353393ac04d3125ba0fa
ChelseaSoto <ch951840@ucf.edu>
2024-02-03 15:15:32 -0500

    Added new sounds and assets. Controls added to scene.

--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -5,23 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
+    public AudioSource buttonClickSound;
+
+    private void Start(){
+        buttonClickSound = GetComponent<AudioSource>();
+    }
+
     public void PlayGame(){
+        PlayButtonClickSound();
         SceneManager.LoadSceneAsync(1);
     }
 
     public void Title(){
+        PlayButtonClickSound();
         SceneManager.LoadSceneAsync(0);
     }
 
     public void Controls(){
+        PlayButtonClickSound();
         SceneManager.LoadSceneAsync(3);
     }
 
     public void Credits(){
+        PlayButtonClickSound();
         SceneManager.LoadSceneAsync(4);
     }
 
     public void QuitGame(){
+        PlayButtonClickSound();
         Application.Quit();
+    }
+
+    private void PlayButtonClickSound(){
+        if (buttonClickSound != null)
+        {
+            buttonClickSound.Play();
+        }
+    }
+
+    private IEnumerator LoadSceneAsync(int sceneIndex){
+        yield return new WaitForSeconds(buttonClickSound.clip.length);
+        SceneManager.LoadSceneAsync(sceneIndex);
     }
 }
